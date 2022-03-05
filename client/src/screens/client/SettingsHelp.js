e8f60605aa699c338b81c75be87c98d6f0805e99
LaurenParsons516 <55518486+LaurenParsons516@users.noreply.github.com>
2022-03-05 16:30:54 -0600

    test

---
+++ client/src/screens/client/SettingsHelp.js
@@ -0,0 +1,14 @@
+import React, {useEffect, useState} from 'react'
+import {Center, VStack, Box, Text, Button, Input, Heading, KeyboardAvoidingView} from 'native-base'
+
+const SettingsHelp = () => {
+    return (
+        <VStack flex={1} safeArea>
+            <Center>
+                <Heading size={'lg'} color={'#000'}>Calendar Screen</Heading>
+            </Center>
+        </VStack>
+    )
+}
+
+export default SettingsHelp
